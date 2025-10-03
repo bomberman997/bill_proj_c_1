c344686a99b700f5e882407b16bb9509b358b368
bomberman997 <bomberman997>
2025-10-03 18:20:34 -0400

    Fix compilation and test issues - remove endianness conversion, fix create_db_header call Still dont know 123

--- src/main.c
+++ src/main.c
@@ -55,32 +55,24 @@ int main(int argc, char *argv[]) {
         return -1;
     }
 
-    if (newfile) {
-        dbfd = create_db_file(filepath);
-        if (dbfd == STATUS_ERROR) {
-            printf("Unable to create database file\n");
-            return -1;
-        }
-
-        // Fixed: Only ONE argument (removed dbfd)
-        if (create_db_header(&dbhdr) == STATUS_ERROR) {
-            printf("Failed to create database header\n"); 
-            return -1;
-        }
-    } else {
-        dbfd = open_db_file(filepath);
-        if (dbfd == STATUS_ERROR) {
-            printf("Unable to open database file\n");
-            return -1;
-        }
-
-        if (validate_db_header(dbfd, &dbhdr) == STATUS_ERROR) {
-            printf("Failed to validate database header\n");
-            return -1;
-        }
+if (newfile) {
+    dbfd = create_db_file(filepath);
+    if (dbfd == STATUS_ERROR) {
+        printf("Unable to create database file\n");
+        return -1;
     }
 
-    if (read_employees(dbfd, dbhdr, &employees) != STATUS_SUCCESS) {
+    if (create_db_header(&dbhdr) == STATUS_ERROR) {
+        printf("Failed to create database header\n"); 
+        return -1;
+    }
+    
+    // ADD THIS: Write the header immediately after creating it
+    if (output_file(dbfd, dbhdr, NULL) == STATUS_ERROR) {
+        printf("Failed to write initial database header\n");
+        return -1;
+    }
+}    if (read_employees(dbfd, dbhdr, &employees) != STATUS_SUCCESS) {
         printf("Failed to read employees\n");
         output_file(dbfd, dbhdr, NULL);
         return -1;
