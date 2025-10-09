30436de5a907785cc531b5cda41aa8843b7b9b39
bomberman997 <bomberman997>
2025-10-09 01:43:04 -0400

    Broken make

--- include/parse.h
+++ include/parse.h
@@ -1,32 +1,31 @@
-#include <stdint.h>
+// include/parse.h
+#ifndef PARSE_H
+#define PARSE_H
+#include "common.h"
 
 #define HEADER_MAGIC 0x4c4c4144
 #define NAME_LEN 256
 #define ADDRESS_LEN 256
 
 struct dbheader_t {
-    uint32_t magic;
-    uint16_t version;
-    uint16_t count;
-    uint32_t filesize;
+    unsigned int   magic;
+    unsigned short version;
+    unsigned short count;
+    unsigned int   filesize;
 };
 
 struct employee_t {
     char name[NAME_LEN];
     char address[ADDRESS_LEN];
-    uint32_t hours;
+    unsigned int hours;
 };
 
-// Function interfaces (as given)
 int create_db_header(struct dbheader_t **headerOut);
 int validate_db_header(int fd, struct dbheader_t **headerOut);
-int read_employees(int fd, struct dbheader_t *dbhdr, struct employee_t **employeesOut);
-
-//int output_file(int fd, struct dbheader_t *dbhdr, struct employee_t *employees);
-// void output_file(int fd,struct dbheader_t *);
-int output_file(int fd, struct dbheader_t *, struct employee_t *employees);  // ← required
-
-// Likely needed elsewhere in your project/tests
+int read_employees(int fd, struct dbheader_t *, struct employee_t **employeesOut);
+int output_file(int fd, struct dbheader_t *, struct employee_t *employees);
 int add_employee(struct dbheader_t *dbhdr, struct employee_t *employees, char *addstring);
 int list_employees(struct dbheader_t *dbhdr, struct employee_t *employees);
+
+#endif
 
